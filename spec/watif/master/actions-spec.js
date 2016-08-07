3d94da0f8bc9e45d422aa633778b6522c15b4b46
jwillesen <git@wizardwell.net>
2016-08-06 20:34:32 -0600

    add and rework player actions

--- spec/watif/master/actions-spec.js
+++ spec/watif/master/actions-spec.js
@@ -4,13 +4,12 @@ import expect from 'expect'
 import * as actions from 'watif/master/actions'
 
 describe('master actions', () => {
-  describe(actions.SELECT_ITEM, () => {
-    it('pulls a description from an item', () => {
-      const universe = {}
-      const item = {description: expect.createSpy().andReturn('a frob')}
-      const action = actions.selectItem(universe, item)
-      expect(item.description).toHaveBeenCalledWith(universe)
-      expect(action.payload).toMatch({item: item, description: 'a frob'})
+  describe(actions.CREATE_ITEM, () => {
+    it('instantiates the class with class name', () => {
+      class SpyClass {}
+      const action = actions.createItem(SpyClass)
+      expect(action.payload.name).toBe('SpyClass')
+      expect(action.payload.item).toBeA(SpyClass)
     })
   })
 })
